6dc41da6fe33070d6c202db6c2612a3e555fb26b
Devindi chathurika <devindichathurika03@gmail.com>
2026-01-11 12:02:14 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter  } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+// main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HashRouter >
-      <App/>
-    </HashRouter>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
